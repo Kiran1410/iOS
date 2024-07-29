2db3c9818d9466e47c62d579fdaa6b1c09546eae
kjoshi25 <kjoshi@ivycomptech.com>
2024-07-30 00:24:34 +0530

    Updated pod file

---
+++ UnityiOSPOC/Pods/Target Support Files/Pods-UnityiOSPOC-UnityiOSPOCUITests/Pods-UnityiOSPOC-UnityiOSPOCUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UnityiOSPOC_UnityiOSPOCUITests : NSObject
+@end
+@implementation PodsDummy_Pods_UnityiOSPOC_UnityiOSPOCUITests
+@end
---
+++ UnityiOSPOC/Pods/Target Support Files/Pods-UnityiOSPOC/Pods-UnityiOSPOC-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UnityiOSPOC : NSObject
+@end
+@implementation PodsDummy_Pods_UnityiOSPOC
+@end
